dc581ef47975fb24dc10c5115a9835bcd13c8941
wangsaisai <wangsaisai@users.noreply.github.com>
2023-12-05 17:35:31 +0800

    Update ExceptionToString.java

--- src/main/java/com/bamboo/util/ExceptionToString.java
+++ src/main/java/com/bamboo/util/ExceptionToString.java
@@ -1,17 +1,11 @@
+package com.bamboo.util;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class ExceptionToStringExample {
-    public static void main(String[] args) {
-        try {
-            // 你的代码，可能会抛出异常
-        } catch (Exception e) {
-            String exceptionAsString = getStackTraceAsString(e);
-            System.out.println(exceptionAsString);
-        }
-    }
 
-    private static String getStackTraceAsString(Exception e) {
+    public static String getStackTraceAsString(Exception e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
